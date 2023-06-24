a906428af64ccb630c14bb8cd7cadde0b9810251
Preetisam <preetikamilla@gmail.com>
2023-06-24 13:57:50 +0530

    languages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react';
+import './App.css'
 function App() {
+  const [languages, setLanguages] = useState([
+    { name: 'Python', count: 0 }, 
+    { name: 'Java', count: 0 },  
+    { name: 'JavaScript', count: 0 },
+    { name: 'C++', count: 0 },  
+    { name: 'PHP', count: 0 }  
+  ]);
+
+  function incrementCount(index) {
+    const newLanguages = [...languages];
+    newLanguages[index].count++;  
+    setLanguages(newLanguages);  
+  }
+
+  function decrementCount(index) {
+    const newLanguages = [...languages];
+    newLanguages[index].count--;  
+    setLanguages(newLanguages);    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="language-list">      
+      {languages.map(({name, count}, index) => (
+        <div key={index} className="language">
+          <span>{name}</span>       
+          <button onClick={() => incrementCount(index)} className="increment">+</button>    
+          <button onClick={() => decrementCount(index)} className="decrement">-</button>      
+          <span className="count">{count}</span>            
+        </div>  
+      ))}
     </div>
-  );
+  );  
 }
 
 export default App;
